8c0c85a7b659ec6fa03272d7917522313feeb67e
iamcollins404 <61983049+iamcollins404@users.noreply.github.com>
2025-11-24 09:51:27 +0000

    Initialized project and files structure and add basic server setup with TypeScript and Express
    I have added more files, empty files to our folder structure
    
    They are awaiting filling in of the flesh and logic

---
+++ api/src/routes/issues.routes.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// the routes we will create and import from controllers
+// 1. create a new issue
+// 2. get all issues
+// 3. get a single issue
+// 4. update an issue
+// 5. delete an issue
+
+module.exports = router;
